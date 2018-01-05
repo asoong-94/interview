9b2d93a2c4dc9d1fe0e3ce9b150639ef8da1fcbc
Alex Soong <asoong@ucdavis.edu>
2018-01-04 22:43:03 -0800

    added right rotation too

--- CTCI/hackerrank/arrayLeftRotate.py
+++ CTCI/hackerrank/arrayLeftRotate.py
@@ -1,12 +1,22 @@
 def array_left_rotation(a, n):
+	"""
+	rotate array a to left n times
+	"""
 	for _ in range(n):
 		a.append(a.pop(0))
 	return a
 
 
+def array_right_rotation(a, n):
+	"""
+	rotate array a to right n times
+	"""
+	for _ in range(n):
+		a.insert(0, a.pop())
+	return a
 
 if __name__ == '__main__':
 	array1 = [1,2,3,4,5]
-	print array1
 	print array_left_rotation(array1, 2)
+	print array_right_rotation(array1, 2)
 
